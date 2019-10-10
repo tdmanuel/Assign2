286d400d7f3b5180979d81b5ab2fd5f1b7d1622a
Thera Manuel <tdmanuel@asu.edu>
2019-10-09 19:28:49 -0700

    Changes made to the class to fill in the empty methods

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -14,28 +14,34 @@ package cse360assign2;
 public class AddingMachine {
 
 	private int total;
+	private String history ="0"; 
 	
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
 	}
 	
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	public void add (int value) {
-		
+		total = total + value;
+		history += " + " + value;
 	}
 	
 	public void subtract (int value) {
-		
+		total = total - value;
+		history += " - " + value; 
 	}
 		
 	public String toString () {
-		return "";
+		
+		return history;
 	}
 
 	public void clear() {
+		total = 0;
+		history = "0";
 	
 	}
 }
