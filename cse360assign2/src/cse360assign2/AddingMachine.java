7a1464d33fa292cde5e3113c65f87146c39ca851
Thera Manuel <tdmanuel@asu.edu>
2019-10-09 19:50:35 -0700

    Adds and subtracts from the total as well as returning the total.

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -3,7 +3,8 @@
  * Class ID: 85141
  * Assignment #2
  * 
- * Description: Adds, subtracts, and returns the total. Provides history of addition and subtraction of the total
+ * Description: Adds, subtracts, and returns the total. Provides history of addition and 
+ * subtraction of the total and clears them
  * 
  * @param A total to be added to or subtracted from
  * @return The resulting total and history of transactions
@@ -16,29 +17,35 @@ public class AddingMachine {
 	private int total;
 	private String history ="0"; 
 	
+	//Constructor to define the variable total
 	public AddingMachine () {
-		total = 0;  // not needed - included for clarity
+		total = 0;
 	}
 	
+	//Returns the resulting total
 	public int getTotal () {
 		return total;
 	}
 	
+	//Adds a given value to the total and adds the transaction to the history.
 	public void add (int value) {
 		total = total + value;
 		history += " + " + value;
 	}
 	
+	//Subtracts a given value from the total and adds the transaction to the history.
 	public void subtract (int value) {
 		total = total - value;
 		history += " - " + value; 
 	}
-		
+	
+	//Returns a string showing the history of transactions.
 	public String toString () {
 		
 		return history;
 	}
 
+	//Clears the total and history back to 0
 	public void clear() {
 		total = 0;
 		history = "0";
