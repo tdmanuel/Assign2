87d707689717019b0ef200e15bce0e38899f866f
Thera Manuel <tdmanuel@asu.edu>
2019-10-09 18:43:04 -0700

    Commit of assignment2, AddingMachine, adds and subtracts from the total.

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,41 @@
+/**
+ * Name: Thera Manuel
+ * Class ID: 85141
+ * Assignment #2
+ * 
+ * Description: Adds, subtracts, and returns the total. Provides history of addition and subtraction of the total
+ * 
+ * @param A total to be added to or subtracted from
+ * @return The resulting total and history of transactions
+ */
+
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+		
+	public String toString () {
+		return "";
+	}
+
+	public void clear() {
+	
+	}
+}
